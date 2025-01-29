2ed9085d4697c767ace6a47e509a7778a016cfcc
chiversg <127253224+chiversg@users.noreply.github.com>
2025-01-29 11:02:43 -0500

    Worked on player movement code
    Just uploading what I managed to update in class

--- Assets/Game/Scripts/Player/SkateboardMovement.cs
+++ Assets/Game/Scripts/Player/SkateboardMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SkateboardMovement : MonoBehaviour
@@ -11,14 +9,19 @@ public class SkateboardMovement : MonoBehaviour
     [SerializeField]
     private float maxManualSpeed;
     [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
     private float speed = 1;
     [SerializeField]
     private float originalStepOffset;
     [SerializeField]
     private float jumpSpeed;
+    [SerializeField]
+    private float drag;
 
     private float direction;
     private float vSpeed;
+    private float xSpeed;
     private float gravStrength = 9.8f;
     private Vector2 moveVector = new Vector2(0f, 0f);
 
@@ -26,6 +29,7 @@ public class SkateboardMovement : MonoBehaviour
     Vector2 dragVelocity;   //The resistance from the ground slowing the player down
     Vector2 momentumGain;   //Extra momemtum gained by gravity
     Vector2 velocity;       //The player's total velocity
+    Vector2 rotatedVelocity; //The player's velocity rotated to slope
     Vector2 gravity;
 
     void Start()
@@ -38,29 +42,43 @@ public class SkateboardMovement : MonoBehaviour
     {
         movePlayer();
         applyGravity();
+        applyDrag();
+        //addMomentum();
+        Debug.Log("Drag: " + dragVelocity);
+        Debug.Log("Velocity:" + velocity);
         velocity = inputVelocity + gravity;
-        velocity = adjustVelocityToSlope(velocity);
+        velocity += dragVelocity;
+        velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+        rotatedVelocity = adjustVelocityToSlope(velocity);
         matchRotation();
-        player.Move(velocity * Time.deltaTime);
+        Debug.Log(player.transform.rotation.x);
+
+        player.Move(rotatedVelocity * Time.deltaTime);
     }
     private void movePlayer()
     {
-        float direction = Input.GetAxis("Horizontal");
-        inputVelocity += new Vector2(direction * speed, 0f);
+        float direction = Input.GetAxisRaw("Horizontal");
+        Debug.Log("Player dir" + direction);
+        inputVelocity += new Vector2(direction * speed * acceleration, 0f);
         inputVelocity.x = Mathf.Clamp(inputVelocity.x, -maxManualSpeed, maxManualSpeed);
     }
     private void applyGravity()
     {
         //Debug.Log(player.isGrounded);
-        if(player.isGrounded == false)
+        if (player.isGrounded == false)
         {
-            vSpeed -= gravStrength * Time.deltaTime; 
-        } 
+            vSpeed -= gravStrength * Time.deltaTime;
+        }
         else
         {
             vSpeed = 0f;
         }
         gravity = new Vector2(0f, vSpeed);
+    }
+    private void applyDrag()
+    {
+        dragVelocity = new Vector2(-velocity.normalized.x * drag, 0f);
+
     }
     private void matchRotation()
     {
@@ -69,18 +87,18 @@ public class SkateboardMovement : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hitInfo, 5f) && hitInfo.collider.gameObject.tag == "Floor")
         {
             player.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-            Debug.Log(hitInfo.collider.gameObject.name);
+            //Debug.Log(hitInfo.collider.gameObject.name);
         }
     }
     private Vector2 adjustVelocityToSlope(Vector2 velocity)
     {
         var ray = new Ray(player.transform.position, player.transform.rotation * Vector2.down);
 
-        if(Physics.Raycast(ray, out RaycastHit hitInfo, 10f) && hitInfo.collider.gameObject.tag == "Floor")
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 10f) && hitInfo.collider.gameObject.tag == "Floor")
         {
             var slopeRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
             var adjustedVelocity = slopeRotation * velocity;
-            return adjustedVelocity;   
+            return adjustedVelocity;
         }
         return velocity;
     }
@@ -93,11 +111,9 @@ public class SkateboardMovement : MonoBehaviour
     }
     private void addMomentum()
     {
-        Vector2 addedMomentum = findComponents(player.transform.rotation.eulerAngles.z, 9.8f); ;
-        addedMomentum.x = addedMomentum.y;
-        addedMomentum.y = 0f;
-        //addedMomentum = rb.rotation * addedMomentum;
-        //rb.AddForce(addedMomentum, ForceMode.Acceleration);
+        Debug.Log("Player Rotation: " + player.transform.rotation.z);
+        momentumGain = findComponents(player.transform.rotation.eulerAngles.z, 9.8f); ;
+        Debug.Log("Horizonal Momentum Gain: " + momentumGain.y);
     }
     private Vector2 findComponents(float angle, float magnitude)
     {
