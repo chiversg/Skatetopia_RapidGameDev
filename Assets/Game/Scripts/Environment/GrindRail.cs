744720a0e47d1dcfd48d3e8b0c9ff7bf598c24e6
SamuelDEFriesen <orangeofthejuicer@gmail.com>
2025-02-03 09:57:53 -0500

    Added rudimentary rail mechanics
    added rail mechanics, updated skateboard movement script to include method to make player move along rail. It likely looks like every file was modified, that's because I had been editing out of a clone, I only edited the railgrind script, the player script, and the test scene

---
+++ Assets/Game/Scripts/Environment/GrindRail.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrindRail : MonoBehaviour
+{
+    public Transform end;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("rail enter");
+        if(other.tag == "Player"){
+            Debug.Log("player enter rail");
+            player.GetComponent<SkateboardMovement>().railEnd = end;
+            player.GetComponent<SkateboardMovement>().onRail = true;
+        }
+    }
+}
